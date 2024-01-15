05f9997336a62b15f6d5f8d340130919f5b622c6
Vincent Toon <vincenttoon22@gmail.com>
2024-01-15 13:32:02 -0600

    registering choices

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
 
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -27,7 +29,10 @@ function App() {
       setTurns(0)
   }
 
-  console.log(cards, turns);
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
@@ -36,7 +41,11 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice}
+          />
         ))}
       </div>
     </div>
